2d0de8edaca6a862219ac42fa3fc5672ac762934
TylerAshton <tyler.ashton-linnett@mail.bcu.ac.uk>
2025-01-13 21:07:03 +0000

    Created Infinite Platform generation

--- JumpBot/PlatformManager.cpp
+++ JumpBot/PlatformManager.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 
+bool startScroll = false;
+
 PlatformManager::PlatformManager(SDL_Renderer* sdlRenderer, Player* Platplayer)
 {
 	renderer = sdlRenderer;
@@ -12,23 +14,16 @@ int PlatformManager::init()
 {
 	srand(time(0));
 
-	int previousY;
-	int previousX;
-
-	int newY;
-	int newX;
-
-	int playerDim = 100;
-
 	for (int i = 0; i < 10; i++)
 	{
-		if (i == 0) {
+		if (i == 0)
+		{
 			addPlat(std::unique_ptr <Platform>(new Platform(renderer, player, SDL_FPoint{ (float)(rand() % (screenResolution::get_screenWidth() + 1 - playerDim)), (float)200 })));
 			platforms[i]->init();
 		}
 		else
 		{
-			previousY = platforms.at(i - 1).get()->collider.y;
+			/*previousY = platforms.at(i - 1).get()->collider.y;
 			previousX = platforms.at(i - 1).get()->collider.x;
 			
 			newX = ( -250 + rand() % 501) + previousX;
@@ -42,7 +37,9 @@ int PlatformManager::init()
 
 			addPlat(std::unique_ptr <Platform>(new Platform(renderer, player, SDL_FPoint{ (float)newX, (float)((previousY + 75 + rand() % (201)))})));
 
-			platforms[i]->init();
+			platforms[i]->init();*/
+
+			generatePlatform();
 		}
 		
 	}
@@ -50,17 +47,61 @@ int PlatformManager::init()
 	return 0;
 }
 
+void PlatformManager::generatePlatform()
+{
+	previousY = platforms.back().get()->collider.y;
+	previousX = platforms.back().get()->collider.x;
+
+	newX = (-250 + rand() % 501) + previousX;
+
+	while ((newX < 0) || (newX > screenResolution::get_screenWidth() - playerDim))
+	{
+		newX = (-250 + rand() % 501) + previousX;
+	}
+
+	addPlat(std::unique_ptr <Platform>(new Platform(renderer, player, SDL_FPoint{ (float)newX, (float)((previousY + 75 + rand() % (201))) })));
+
+	platforms.back()->init();
+}
+
 void PlatformManager::addPlat(std::unique_ptr<Platform> platapus)
 {
 	platforms.push_back(std::move(platapus));
 }
+
+
+
+void PlatformManager::scroll()
+{
+	for (auto& plat : platforms)
+	{
+		plat.get()->collider.y -= 0.025f;
+	}
+}
+
+
 
 void PlatformManager::update()
 {
 	for (auto& plat : platforms)
 	{
 		plat->update();
+		if (plat.get()->collider.y < -50)
+		{
+			platforms.pop_front();
+			generatePlatform();
+		}
 	}
+
+	if (player->get_playerPos().y > 300)
+	{
+		startScroll = true;
+	}
+
+	if (startScroll) {
+		scroll();
+	}
+
 }
 
 void PlatformManager::render()
--- JumpBot/PlatformManager.h
+++ JumpBot/PlatformManager.h
@@ -1,21 +1,33 @@
 #pragma once
 
 #include <vector>
+#include <deque>
 #include "Platform.h"
 
 class PlatformManager
 {
 public:
 	PlatformManager(SDL_Renderer* sdlRenderer, Player* Platplayer);
-	std::vector<std::unique_ptr<Platform>> platforms = {};
+	//std::vector<std::unique_ptr<Platform>> platforms = {};
+	std::deque<std::unique_ptr<Platform>> platforms = {};
 
 	int init();
 	void update();
 	void render();
 	void addPlat(std::unique_ptr<Platform> plats);
 
+	void generatePlatform();
+
+	void scroll();
+
 private:
+	int previousY;
+	int previousX;
+	int newY;
+	int newX;
+
+	int playerDim = 100;
+
 	Player* player;
 	SDL_Renderer* renderer = nullptr;
 };
-
